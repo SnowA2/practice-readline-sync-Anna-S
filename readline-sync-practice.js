5df281b03057569a4c7ce3b0cd63fb6397d149b3
SnowA2 <snowqueen120@gmail.com>
2025-10-01 21:13:03 -0500

    Questions 1-5 added

--- readline-sync-practice.js
+++ readline-sync-practice.js
@@ -4,3 +4,14 @@ let name = readline.question("What is your name? ");
 
 console.log("Hello, " + name + "!");
 
+let Q1 = readline.question("What is a String?");
+
+let Q2 = readline.question("What is a Variable?");
+
+let Q3 = readline.questionInt("Pick a number from 1 - 10: ");
+
+console.log("That's a nice number.");
+
+let Q4 = readline.question("What does 'var' mean to you?");
+
+let Q5 = readline.question("How do you convert a string to a number?");
